9ebac765ac852e71aee38c73aa54f0955f82c4cd
David Guerin <david.guerin@antheainteractive.com>
2014-10-03 14:34:53 +0200

    deletion of acme demo bundle

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit5991a4d80f3242c362bea1a93b35d29a::getLoader();
+return ComposerAutoloaderInit3526138369b85f3e208ed3fe2dd7b44b::getLoader();
